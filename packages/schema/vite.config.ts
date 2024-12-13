9be995e5ebb567a56f27d0834103e57e13c87237
1604549268@qq.com <1604549268@qq.com>
2024-12-13 11:14:41 +0800

    feat: add lowcode schema

---
+++ packages/schema/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    dts({
+      outDir: 'dist/types',
+      include: ['src/**/*'],
+      staticImport: true,
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    sourcemap: true,
+
+    lib: {
+      entry: 'src/index.ts',
+      name: 'LowCodeSchema',
+      fileName: 'lowcode-schema',
+    },
+  },
+});
