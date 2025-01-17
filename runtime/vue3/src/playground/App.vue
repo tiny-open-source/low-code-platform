dfe1106e3ae77b5ace6e020128c4506a0e5fc346
1604549268@qq.com <1604549268@qq.com>
2025-01-17 17:57:02 +0800

    fix(runtime): lowcodeRuntimeReady时机修改

--- runtime/vue3/src/playground/App.vue
+++ runtime/vue3/src/playground/App.vue
@@ -3,7 +3,7 @@ import type { Id, MApp, MNode } from '@lowcode/schema';
 import type { LowCode, RemoveData, UpdateData } from '@lowcode/stage';
 import Core from '@lowcode/core';
 import { getNodePath } from '@lowcode/utils';
-import { computed, nextTick, onMounted, provide, reactive, ref, watch } from 'vue';
+import { computed, nextTick, provide, reactive, ref, watch } from 'vue';
 
 declare global {
   interface Window {
@@ -34,73 +34,71 @@ watch(pageConfig, async () => {
   page && window.lowcode.onPageElUpdate(page);
 });
 
-onMounted(() => {
-  window.lowcode?.onRuntimeReady({
-    getApp() {
-      return app;
-    },
-    updateRootConfig(config: MApp) {
-      console.log('update config', config);
-      root.value = config;
-    },
-    updatePageId(id: Id) {
-      console.log('update page id', id);
-      curPageId.value = id;
-      app?.setPage(id);
-    },
-    getSnapElements() {
-      return Array.from(document.querySelectorAll<HTMLElement>('[class*=magic-ui][id]'));
-    },
-    select(id: Id) {
-      console.log('select config', id);
-      selectedId.value = id;
-      const el = document.getElementById(`${id}`);
-      if (el)
-        return el;
-      return nextTick().then(() => document.getElementById(`${id}`) as HTMLElement);
-    },
-    add({ config }: UpdateData) {
-      console.log('add config', config);
-      if (!root.value)
-        throw new Error('error');
-      if (!selectedId.value)
-        throw new Error('error');
-      const path = getNodePath(selectedId.value, [root.value]);
-      const node = path.pop();
-      const parent = node?.items ? node : path.pop();
-      if (!parent)
-        throw new Error('未找到父节点');
-        // 当前选中节点作为父节点
-      parent.items?.push(config);
-    },
-    update({ config }: UpdateData) {
-      console.log('update config', config);
-      if (!root.value)
-        throw new Error('error');
-      const path = getNodePath(config.id, [root.value]);
-      const node = path.pop();
-      const parent = path.pop();
-      if (!node)
-        throw new Error('未找到目标节点');
-      if (!parent)
-        throw new Error('未找到父节点');
-      const index = parent.items?.findIndex((child: MNode) => child.id === node.id);
-      parent.items.splice(index, 1, reactive(config));
-    },
-    remove({ id }: RemoveData) {
-      if (!root.value)
-        throw new Error('error');
-      const path = getNodePath(id, [root.value]);
-      const node = path.pop();
-      if (!node)
-        throw new Error('未找到目标元素');
-      const parent = path.pop();
-      if (!parent)
-        throw new Error('未找到父元素');
-      const index = parent.items?.findIndex((child: MNode) => child.id === node.id);
-      parent.items.splice(index, 1);
-    },
-  });
+window.lowcode?.onRuntimeReady({
+  getApp() {
+    return app;
+  },
+  updateRootConfig(config: MApp) {
+    console.log('update config', config);
+    root.value = config;
+  },
+  updatePageId(id: Id) {
+    console.log('update page id', id);
+    curPageId.value = id;
+    app?.setPage(id);
+  },
+  getSnapElements() {
+    return Array.from(document.querySelectorAll<HTMLElement>('[class*=magic-ui][id]'));
+  },
+  select(id: Id) {
+    console.log('select config', id);
+    selectedId.value = id;
+    const el = document.getElementById(`${id}`);
+    if (el)
+      return el;
+    return nextTick().then(() => document.getElementById(`${id}`) as HTMLElement);
+  },
+  add({ config }: UpdateData) {
+    console.log('add config', config);
+    if (!root.value)
+      throw new Error('error');
+    if (!selectedId.value)
+      throw new Error('error');
+    const path = getNodePath(selectedId.value, [root.value]);
+    const node = path.pop();
+    const parent = node?.items ? node : path.pop();
+    if (!parent)
+      throw new Error('未找到父节点');
+    // 当前选中节点作为父节点
+    parent.items?.push(config);
+  },
+  update({ config }: UpdateData) {
+    console.log('update config', config);
+    if (!root.value)
+      throw new Error('error');
+    const path = getNodePath(config.id, [root.value]);
+    const node = path.pop();
+    const parent = path.pop();
+    if (!node)
+      throw new Error('未找到目标节点');
+    if (!parent)
+      throw new Error('未找到父节点');
+    const index = parent.items?.findIndex((child: MNode) => child.id === node.id);
+    parent.items.splice(index, 1, reactive(config));
+  },
+  remove({ id }: RemoveData) {
+    if (!root.value)
+      throw new Error('error');
+    const path = getNodePath(id, [root.value]);
+    const node = path.pop();
+    if (!node)
+      throw new Error('未找到目标元素');
+    const parent = path.pop();
+    if (!parent)
+      throw new Error('未找到父元素');
+    const index = parent.items?.findIndex((child: MNode) => child.id === node.id);
+    parent.items.splice(index, 1);
+  },
 });
 </script>
 
@@ -111,6 +109,9 @@ onMounted(() => {
 </template>
 
 <style lang="scss">
+::-webkit-scrollbar {
+  width: 0;
+}
 html,
 body,
 #app {
@@ -130,8 +131,5 @@ body,
   position: relative;
   overflow: auto;
 
-  &::-webkit-scrollbar {
-    width: 0;
-  }
 }
 </style>
