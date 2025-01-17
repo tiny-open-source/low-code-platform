aabe9de491001fc62e8e539ba5b5718a3bb98666
1604549268@qq.com <1604549268@qq.com>
2025-01-17 11:34:50 +0800

    fix(form): fieldset checkbox change事件不会触发

--- packages/form/src/container/Fieldset.vue
+++ packages/form/src/container/Fieldset.vue
@@ -1,69 +1,71 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import type { FieldsetConfig, FormState } from '../schema';
+
 import { NCheckbox } from 'naive-ui';
-import { computed, inject, type PropType } from 'vue';
+import { computed, inject, watch } from 'vue';
 
 defineOptions({
   name: 'l-form-fieldset',
 });
-const props = defineProps({
-  labelWidth: String,
-  model: {
-    type: Object,
-    default: () => ({}),
-  },
 
-  config: {
-    type: Object as PropType<FieldsetConfig>,
-    default: () => ({}),
+const props = withDefaults(
+  defineProps<{
+    labelWidth?: string;
+    prop: string;
+    size?: string;
+    model: Record<string, any>;
+    config: FieldsetConfig;
+    rules?: any;
+  }>(),
+  {
+    rules: {},
+    prop: '',
   },
+);
 
-  prop: {
-    type: String,
-    default: () => '',
-  },
-
-  size: String,
-});
 const emit = defineEmits(['change']);
+
 const lForm = inject<FormState | undefined>('lForm');
+
 const name = computed(() => props.config.name || '');
+
 const show = computed(() => {
   if (props.config.expand && name.value) {
     return props.model[name.value]?.value;
   }
   return true;
 });
+
 const lWidth = computed(() => {
   if (props.config.items) {
     return props.config.labelWidth || props.labelWidth;
   }
   return props.config.labelWidth || props.labelWidth || (props.config.text ? null : '0');
 });
-const key = (item: any, index: number) => item[lForm?.keyProp || '__key'] ?? index;
+
 function change() {
   emit('change', props.model);
+}
+
+const key = (item: any, index: number) => item[lForm?.keyProp || '__key'] ?? index;
+
+if (props.config.checkbox && name.value) {
+  watch(
+    () => props.model[name.value]?.value,
+    () => {
+      emit('change', props.model);
+    },
+  );
 }
 </script>
 
 <template>
   <fieldset
-    v-if="name ? model[name] : model" class="l-fieldset"
+    v-if="name ? model[name] : model"
+    class="l-fieldset"
     :style="show ? 'padding: 15px 15px 0 5px;' : 'border: 0'"
   >
-    <NCheckbox
-      v-if="!show && name"
-      v-model:checked="
-        //eslint-disable-next-line vue/no-mutating-props
-        model[name].value"
-      :prop="`${prop}${prop ? '.' : ''}${config.name}.value`"
-      :true-label="1"
-      :false-label="0"
-      @change="change"
-    >
-      <span v-html="config.legend" /><span v-if="config.extra" class="l-form-tip" v-html="config.extra" />
-    </NCheckbox>
-    <legend v-else-if="config.checkbox && name">
+    <component :is="!show ? 'div' : 'legend'" v-if="name && config.checkbox">
       <NCheckbox
         v-model:checked="
           //eslint-disable-next-line vue/no-mutating-props
@@ -71,11 +73,10 @@ function change() {
         :prop="`${prop}${prop ? '.' : ''}${config.name}.value`"
         :true-label="1"
         :false-label="0"
-        @change="change"
       >
         <span v-html="config.legend" /><span v-if="config.extra" class="l-form-tip" v-html="config.extra" />
       </NCheckbox>
-    </legend>
+    </component>
     <legend v-else>
       <span v-html="config.legend" />
       <span v-if="config.extra" class="l-form-tip" v-html="config.extra" />
@@ -86,6 +87,7 @@ function change() {
           v-for="(item, index) in config.items"
           :key="key(item, index)"
           :model="name ? model[name] : model"
+          :rules="name ? rules[name] : []"
           :config="item"
           :prop="prop"
           :label-width="lWidth"
@@ -102,6 +104,7 @@ function change() {
         v-for="(item, index) in config.items"
         :key="key(item, index)"
         :model="name ? model[name] : model"
+        :rules="name ? rules[name] : []"
         :config="item"
         :prop="prop"
         :label-width="lWidth"
