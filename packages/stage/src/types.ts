a4d65c107674642c9c7575610a1d669be9e83205
huangyan321 <1604549268@qq.com>
2025-01-26 20:25:29 +0800

    feat(designer,stage): runtime-api中的add/update/remove中的参数加上parentId

--- packages/stage/src/types.ts
+++ packages/stage/src/types.ts
@@ -1,6 +1,6 @@
 import type Core from '@lowcode/core';
 
-import type { Id, MApp, MContainer, MNode } from '@lowcode/schema';
+import type { Id, MApp, MNode } from '@lowcode/schema';
 import type { MoveableOptions } from 'moveable';
 
 import type { GuidesType } from './const';
@@ -101,12 +101,13 @@ export interface SortEventData {
 
 export interface UpdateData {
   config: MNode;
+  parentId: Id;
   root: MApp;
-  parent?: MContainer;
 }
 
 export interface RemoveData {
   id: Id;
+  parentId: Id;
   root: MApp;
 }
 
